f5cd1582eb3e4f6d3b68c5a9a398fee0e4b75549
Hasibur Rahman <hasib.webdev@gmail.com>
2024-01-01 15:10:13 +0600

    Updates README and added github icon (#1)
    * Update README.md with project setup instructions
    
    * Add Tauri documentation link to README.md
    
    * Refactor service buttons and add GitHub link
    
    * Remove unused code and update service URLs
    
    * Fix link to GitHub repository

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# DOKAN LOCAL
+# Dokan Local
+this is a [Rust Tauri](https://tauri.app) project bootstrapped with [`pnpm create tauri-app`](https://tauri.app/v1/guides/getting-started/setup).
+
+## Getting Started
+
+First install the dependencies:
+```bash
+pnpm install
+```
+
+Run the development server:
+```bash
+pnpm tauri dev
+```
+It will open a new window with your Tauri application.
+You can now edit your Tauri project and see the changes in real-time.
+
+
+## Learn more
+To learn more about Tauri, take a look at the following resources:
+- [Tauri documentation](https://tauri.app/)
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -13,39 +13,49 @@
 
     <fwb-accordion open-first-item always-open class="mt-6">
       <fwb-accordion-panel v-for="service in Object.values(Service)">
-        <fwb-accordion-header
-          >{{ service }}:{{ ports[service] }}
-          <fwb-button
-            v-if="!runningServices.hasOwnProperty(service)"
-            size="xs"
-            @click.stop="startService(service)"
-            color="light"
-            :disabled="!appState.rootDir"
-            >Start</fwb-button
-          >
-          <template v-else>
-            <fwb-badge class="!inline-block" size="xs" type="green"
-              >Running</fwb-badge
-            >
-            <fwb-badge class="!inline-block" size="xs"
-              >PID: {{ runningServices[service] }}</fwb-badge
-            >
+        <fwb-accordion-header>
+          <div class="flex items-center">
+            {{ service }}:{{ ports[service] }}
             <fwb-button
-              class="!inline-block"
+              v-if="!runningServices.hasOwnProperty(service)"
               size="xs"
-              @click.stop="stopService(service)"
-              color="red"
-              >Stop</fwb-button
+              @click.stop="startService(service)"
+              color="light"
+              :disabled="!appState.rootDir"
+              >Start</fwb-button
             >
+            <template v-else>
+              <fwb-badge class="!inline-block" size="xs" type="green"
+                >Running</fwb-badge
+              >
+              <fwb-badge class="!inline-block" size="xs"
+                >PID: {{ runningServices[service] }}</fwb-badge
+              >
+              <fwb-button
+                class="!inline-block"
+                size="xs"
+                @click.stop="stopService(service)"
+                color="red"
+                >Stop</fwb-button
+              >
 
-            <fwb-button
-              size="xs"
-              @click.stop="restartService(service)"
-              color="yellow"
-              class="ml-3"
-              >Restart</fwb-button
+              <fwb-button
+                size="xs"
+                @click.stop="restartService(service)"
+                color="yellow"
+                class="ml-3"
+                >Restart</fwb-button
+              >
+            </template>
+
+            <button
+              @click.stop="linkOpen(`https://github.com/getdokan/${service}`)"
+              target="_blank"
+              class="inline-block w-6 ml-auto mr-2"
             >
-          </template>
+              <github-icon />
+            </button>
+          </div>
         </fwb-accordion-header>
         <fwb-accordion-content
           class="font-fira-code bg-gray-100 overflow-x-auto"
@@ -58,6 +68,7 @@
 </template>
 
 <script setup lang="ts">
+import GithubIcon from "../components/icons/GithubIcon.vue";
 import {
   FwbAccordion,
   FwbAccordionContent,
@@ -77,6 +88,7 @@ import { LogMessage, ServiceStart, ServiceStop } from "../types/service.ts";
 import { Terminal } from "xterm";
 import { useAppStore } from "../stores/app.ts";
 import { setRootDir } from "../persist-state.ts";
+import { open as linkOpen } from "@tauri-apps/api/shell";
 
 const appState = useAppStore();
 
